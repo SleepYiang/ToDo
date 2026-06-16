5b6faa5127f20b8c34fa8853771b4c9e915fca3f
sleepyiang <yiang_1970@163.com>
2026-06-16 21:09:41 +0800

    ToDo项目学习

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+#[derive(Debug)]
+struct Todo{
+    id:u32,
+    title:String,
+    completed:bool
+}
+fn list(todos:&[Todo]){
+    for todo in todos{
+        let status =if todo.completed{
+            "[X]已完成"
+        }else{
+            "[]未完成"
+        };
+    println!("{:<5} {:<10} {}",
+    todo.id,
+    status,
+    todo.title);
+    }
+    
+}
+fn delete(todo:&mut Vec<Todo>){
+    let pos=todo.iter().position(|t|t.id==2);
+    if let Some(idx)=pos{
+        todo.remove(idx);
+        println!("已删除id=2的任务");
+    }else{
+        println!("未找到id=2的任务");
+    }
+}
+fn main(){
+    // let todo1=Todo{
+    // id:1,
+    // title:String::from("学习 rust"),
+    // completed:false
+    // };
+    
+    // println!("{:?}",todo1);
+    let mut todos: Vec<Todo> = Vec::new();
+    todos.push(Todo { 
+        id: 1, 
+        title: "study".to_string(), 
+        completed:false 
+    });
+    todos.push(Todo { 
+        id: 2, 
+        title: "锻炼".to_string(), 
+        completed: false 
+    });
+     todos.push(Todo { 
+        id: 3, 
+        title: "RushB".to_string(), 
+        completed: false 
+    });
+    //循环遍历打印出来
+    // for i in &todos{
+    //     //println!("{:?}",i)
+    //     }
+    // let result=todos.iter().find(|t|t.id==2);
+    // match result {
+    //         Some(i)=> {
+    //         println!("找到任务: {:?}", i);
+    //     }
+    //         None=> {
+    //             println!("未找到任务");
+    //         }
+    //     }
+    let result=todos.iter_mut().find(|t|t.id==2);
+    match result{
+        Some(i)=>{
+            println!("找到任务");
+            i.completed=!i.completed;
+            println!("更改后的内容{:?}",i)
+        }
+        None=>{
+            println!("未找到任务");
+        }
+    }
+
+    list(&todos);
+    delete(&mut todos);
+    list(&todos);
+
+}
+
