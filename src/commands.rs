6059e79047905568a5507d5adca0eea528331b5d
sleepyiang <yiang_1970@163.com>
2026-06-18 21:44:13 +0800

    代码模块

---
+++ src/commands.rs
@@ -0,0 +1,29 @@
+use crate::models::Todo;
+//完成
+pub fn done(todos:&mut Vec<Todo>,id:u32)
+{
+
+}
+
+//显示
+pub fn list(todos:&[Todo])
+{
+
+}
+
+//添加
+pub fn add(todos:&mut Vec<Todo>,id:u32,title:String)
+{
+
+}
+
+//清除
+pub fn clear(todos:&mut Vec<Todo>)
+{
+
+}
+
+
+//查找
+
+//编辑
