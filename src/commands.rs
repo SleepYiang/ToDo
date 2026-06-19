5382d31f50baa4f7ebd5bcd98b9a2b397cefbca9
sleepyiang <yiang_1970@163.com>
2026-06-19 15:41:14 +0800

    模块化处理完成

--- src/commands.rs
+++ src/commands.rs
@@ -1,29 +1,116 @@
 use crate::models::Todo;
+//公共打印函数
+fn print_todo(todo:&Todo)
+{
+    let status =
+        if todo.completed
+        {
+            "[X]已完成"
+        }
+        else
+        {
+            "[ ]未完成"
+        };
+
+    println!(
+        "{:<5} {:<10} {}",
+        todo.id,
+        status,
+        todo.title
+    );
+}
+
 //完成
 pub fn done(todos:&mut Vec<Todo>,id:u32)
 {
-
+    let pos=todos.iter_mut().find(|t|t.id==id);
+    match pos{
+        Some(todo)=>{
+            todo.completed=!todo.completed;
+           
+        }
+        None=>{
+            println!("未找到相关内容");
+        }
+    }
 }
 
 //显示
 pub fn list(todos:&[Todo])
 {
-
+    if todos.is_empty(){
+        println!("当前没有任务");
+        return;
+    }
+    println!("{:<5} {:<12} {}", "ID", "STATUS", "TITLE");
+    println!("--------------------------------");
+    for todo in todos{
+      print_todo(todo);
+    }
 }
 
 //添加
 pub fn add(todos:&mut Vec<Todo>,id:u32,title:String)
 {
-
+    let new_todo=Todo{
+        id,
+        title,
+        completed:false
+    };
+    todos.push(new_todo);
+    println!("新增任务成功，id:{}", id);
 }
 
+
+//删除
+pub fn delete(todos:&mut Vec<Todo>,id:u32)
+{
+    let pos=todos.iter().position(|t|t.id==id);
+    if let Some(indx)=pos{
+        todos.remove(indx);
+        println!("已删除{}的任务",id);
+    }else{
+        println!("未找到{}的任务",id);
+    }
+}
 //清除
 pub fn clear(todos:&mut Vec<Todo>)
 {
-
+    todos.retain(|todo|!todo.completed);
+    println!("=== 已清空完成任务 ===");
 }
 
 
 //查找
+pub fn search(todos:&[Todo],keyword:&str)
+{
+    let result_list:Vec<&Todo>=todos.iter()
+        .filter(|todo|todo.title.contains(keyword))
+        .collect();
+    if result_list.is_empty(){
+        println!("未找到相关内容")
+    }else{
+        println!("===== 关键词「{}」搜索结果 =====", keyword);
+        for todo in result_list {
+            print_todo(todo);
+        }
+    }
+}
 
 //编辑
+pub fn edit(todos:&mut Vec<Todo>,
+        id:u32,
+        new_title:&str)
+{
+      
+    let pos=todos.iter_mut().find(|t|t.id==id);
+    if let Some(todo)=pos{
+        todo.title=new_title.to_string();
+         println!("修改成功，id{}标题已更新", id);
+    }
+    else{
+        println!("未找到id={}的任务，修改失败", id);
+    }
+    
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,9 @@
-#[derive(Debug)]
-struct Todo{
-    id:u32,
-    title:String,
-    completed:bool
-}
-//显示
-fn list(todos:&[Todo]){
-       if todos.is_empty() {
-        println!("当前没有任务");
-        return;
-    }
-    println!("{:<5} {:<12} {}", "ID", "STATUS", "TITLE");
-    println!("--------------------------------");
-    for todo in todos{
-        let status =if todo.completed{
-            "[X]已完成"
-        }else{
-            "[]未完成"
-        };
-    println!("{:<5} {:<10} {}",
-    todo.id,
-    status,
-    todo.title);
-    }
-    
-}
-
-//删除
-fn delete(todos:&mut Vec<Todo>,
-          id:u32)
-    {
-    let pos=todos.iter().position(|t|t.id==id);
-    if let Some(idx)=pos{
-        todos.remove(idx);
-        println!("已删除{}的任务",id);
-    }else{
-        println!("未找到{}的任务",id);
-    }
-}
-//完成
-fn done(todos:&mut Vec<Todo>,
-        id:u32)
-    {
-    let result=todos.iter_mut().find(|t|t.id==id);
-    match result{
-        Some(i)=>{
-            println!("找到任务");
-            i.completed=!i.completed;
-            println!("更改后的内容{:?}",i)
-        }
-        None=>{
-            println!("未找到任务");
-        }
-    }
-}
-
-//修改
-fn edit(todos:&mut Vec<Todo>,
-        id:u32,
-        new_title:String)
-    {
-        let pos=todos.iter_mut().find(|t|t.id==id);
-        if let Some(todo)=pos{
-            todo.title=new_title;
-             println!("修改成功，id{}标题已更新", id);
-        }
-        else{
-             println!("未找到id={}的任务，修改失败", id);
-        }
-    }
-
-//清空
-fn clear(todos:&mut Vec<Todo>)
-{
-    todos.retain(|t|!t.completed);
-    println!("=== 已清空完成任务 ===");
-}
-
-//查找
-fn search(todos: &[Todo], keyword: &str) {
-    // 过滤出标题包含关键词的所有Todo
-    let result_list: Vec<&Todo> = todos.iter()
-        .filter(|todo| todo.title.contains(keyword))
-        .collect();
-
-    if result_list.is_empty() {
-        println!("未找到包含「{}」的任务", keyword);
-    } else {
-        println!("===== 关键词「{}」搜索结果 =====", keyword);
-        for todo in result_list {
-            let status = if todo.completed { "[X]已完成" } else { "[]未完成" };
-            println!("{:<5} {:<10} {}", todo.id, status, todo.title);
-        }
-    }
-} 
-
-//添加任务
-fn add(todos:&mut Vec<Todo>,id:u32,title:String)
-{
-    let new_todo=Todo{
-        id,
-        title,
-        completed:false
-    };
-    todos.push(new_todo);
-    println!("新增任务成功，id:{}", id);
-}
-   
+mod models;
+mod commands;
+use models::Todo;
+use commands::*;   
 fn main(){
-    // let todo1=Todo{
-    // id:1,
-    // title:String::from("学习 rust"),
-    // completed:false
-    // };
-    
-    // println!("{:?}",todo1);
+
     let mut todos: Vec<Todo> = Vec::new();
     todos.push(Todo { 
         id: 1, 
@@ -131,26 +20,18 @@ fn main(){
         title: "RushB".to_string(), 
         completed: false 
     });
-    //循环遍历打印出来
-    // for i in &todos{
-    //     //println!("{:?}",i)
-    //     }
-    // let result=todos.iter().find(|t|t.id==2);
-    // match result {
-    //         Some(i)=> {
-    //         println!("找到任务: {:?}", i);
-    //     }
-    //         None=> {
-    //             println!("未找到任务");
-    //         }
-    //     }
-   
 
     list(&todos);
-    delete(&mut todos,2);
-    list(&todos);
+    //delete(&mut todos,2);
+    //list(&todos);
     done(&mut todos, 1);
-    edit(&mut todos, 3, String::from("test"));
+    edit(&mut todos, 3, "test");
     list(&todos);
+    add(&mut todos,4,"学习".to_string());
+    list(&todos);
+    delete(&mut todos, 2);
+    list(&todos);
+    search(&todos, "学习");
+
 }
 
